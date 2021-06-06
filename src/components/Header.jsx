1e1d3f4be5e515f75cce18647b117032b566f7fd
tilakraj34048 <66312942+tilak34048@users.noreply.github.com>
2021-06-06 16:04:36 +0530

    React Note Keeping web app poject

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+function Header() {
+    return (
+    <header>
+    <h1>
+    <EventNoteIcon />
+    Note Keeper</h1>
+    </header>
+    );
+}
+
+export default Header;
